94cc33f0a6213425a1cca6baffecb77bc70be372
linyuelin <linyuelin1997@gmail.com>
2024-01-25 23:27:14 +0900

    no message

---
+++ CollectionCompositeLayout/CollectionCompositeLayout/ViewController.swift
@@ -0,0 +1,73 @@
+//
+//  ViewController.swift
+//  CollectionCompositeLayout
+//
+//  Created by dreaMTank on 2024/01/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController ,UICollectionViewDataSource {
+    
+   
+    
+    
+    private let collectionView =  UICollectionView (
+       frame: .zero,
+       collectionViewLayout: ViewController.createLayout()
+    )
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.addSubview(collectionView)
+        collectionView.frame = view.bounds
+        collectionView.register(MyCollectionViewCell.self, forCellWithReuseIdentifier: MyCollectionViewCell.identifier)
+        collectionView.backgroundColor = .white
+        collectionView.dataSource = self
+       
+    }
+    
+   static func createLayout() -> UICollectionViewCompositionalLayout {
+         // Item
+       let item = NSCollectionLayoutItem(layoutSize: NSCollectionLayoutSize(widthDimension: .fractionalWidth(2/3), heightDimension: .fractionalHeight(1)))
+      
+       item.contentInsets = NSDirectionalEdgeInsets(top: 2, leading: 2, bottom: 2, trailing: 2)
+       
+       let verticalStackItem = NSCollectionLayoutItem(layoutSize: NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .fractionalHeight(0.5)))
+       
+       verticalStackItem.contentInsets = NSDirectionalEdgeInsets(top: 2, leading: 2, bottom: 2, trailing: 2)
+      
+       let verticalStackGroup = NSCollectionLayoutGroup.vertical(layoutSize: NSCollectionLayoutSize(widthDimension: .fractionalWidth(1/3), heightDimension: .fractionalHeight(1)) , subitem: verticalStackItem, count: 2)
+       
+       let tripletItem = NSCollectionLayoutItem(layoutSize: NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .fractionalWidth(1)))
+       
+       tripletItem.contentInsets = NSDirectionalEdgeInsets(top: 2, leading: 2, bottom: 2, trailing: 2)
+       
+       
+       let tripletHorizontalGroup = NSCollectionLayoutGroup.horizontal(layoutSize: NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .fractionalWidth(0.3)), subitem: tripletItem, count: 3 )
+       
+         //Group
+       let horizontalGroup = NSCollectionLayoutGroup.horizontal(layoutSize: NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .fractionalHeight(0.7)), subitems: [ item , verticalStackGroup ] )
+                 
+       let verticalGroup = NSCollectionLayoutGroup.vertical(layoutSize: NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .fractionalHeight(1.0)), subitems: [  horizontalGroup ,tripletHorizontalGroup
+       ])
+         //Sections
+       let section = NSCollectionLayoutSection(group: verticalGroup)
+       
+         //Return
+       return UICollectionViewCompositionalLayout(section: section)
+    }
+
+
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        30
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        collectionView.dequeueReusableCell(withReuseIdentifier: MyCollectionViewCell.identifier, for: indexPath)
+    }
+}
+
